7b50309d4702b21bfb27736de53bffe8ca3709b5
ACnoway <imlzhyt@163.com>
2022-11-06 13:22:51 +0800

    ./文件编码

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-#from VipCode import *
+# from VipCode import *
 import requests
-import threading
 from bs4 import BeautifulSoup
 import re
 import os
@@ -17,21 +16,24 @@ def gett(url):
     f = open("testt.txt", "a+", encoding='utf-8')
     r = requests.get(url=url, headers=head)  # requests标准形式
     soup = BeautifulSoup(r.text, "html.parser")
-    print((re.findall("<title>(.*?)_铅笔小说",str(soup.select('head title')[0]))[0]),end='')
-    if url.find("_")==-1:
-        f.write('\n'+(re.findall("<title>(.*?)_铅笔小说",str(soup.select('head title')[0]))[0])+"\n\n")
-    temp = soup.select(
-        '.bg6 .mlfy_main #mlfy_main_text p'
-    )
-    print("...",end='')
-    for x in range(0,len(temp)-1):
-        ss=str(temp[x])
-        ss=ss.replace('<p>','')
-        ss=ss.replace('</p>','\n')
+    print((re.findall("<title>(.*?)_铅笔小说", str(
+        soup.select('head title')[0]))[0]),
+          end='')
+    if url.find("_") == -1:
+        f.write('\n' + (re.findall("<title>(.*?)_铅笔小说",
+                                   str(soup.select('head title')[0]))[0]) +
+                "\n\n")
+    temp = soup.select('.bg6 .mlfy_main #mlfy_main_text p')
+    print("...", end='')
+    for x in range(0, len(temp) - 1):
+        ss = str(temp[x])
+        ss = ss.replace('<p>', '')
+        ss = ss.replace('</p>', '\n')
         f.write(ss)
-    print("...",end='')
+    print("...", end='')
     print("已完成")
-    return re.findall("var nextpage=\"(.*?)\"",r.text)[0]
+    return re.findall("var nextpage=\"(.*?)\"", r.text)[0]
+
 
 url = "https://www.23qb.com/book/2385/3335714.html"
 a = gett(url)
--- 文件编码.py
+++ 文件编码.py
@@ -3,12 +3,16 @@ import os
 path = './'
 filelist = os.listdir(path)
 for file in filelist:
-    print(file)
     if file == '.git':
         continue
+    print(file)
     if os.path.isdir(path + file):
         ffilelist = os.listdir(path + file)
         for ffile in ffilelist:
             filelist.append(file + '/' + ffile)
         continue
-    f = open(path + file, "r")
+    name, ext = file.split('.               ')
+    if ext == "exe":
+        continue
+    f = open(path + file, "r", encoding="utf-8")
+    print(f.readline())
