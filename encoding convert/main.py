b6f7a33a20cc030b00317aee885cbe65fea4f24e
ACnoway <imlzhyt@163.com>
2023-03-30 22:04:46 +0800

    encoding convert/main.py

--- encoding convert/main.py
+++ encoding convert/main.py
@@ -1,39 +1,57 @@
 import os
-import codecs
 
 list_name = []
 
 path = os.path.join(os.getcwd(), "shuju")
-print(path)
+out_path = os.path.join(os.getcwd(), "out")
+try:
+    os.mkdir(out_path)
+except FileExistsError:
+    pass
+print(out_path)
+
+# 要统一转换成的编码
+print("choose one")
+print("1. utf-8")
+print("2. gb2312")
+print("please input: ")
+encodes = ['utf-8', 'gb2312']
+encode_in = ""
+encode_out = encodes[int(input()) - 1]
+if encode_out == 'utf-8':
+    encode_in = 'gb2312'
+else:
+    encode_in = 'utf-8'
+print(encode_out)
 
 
-def get_files(dir_path):
+def get_files(dir_path, second):
+    global encode_in, encode_out
     for file in os.listdir(dir_path):
         file_path = os.path.join(dir_path, file)
         if os.path.isdir(file_path):
-            get_files(file_path)
+            second = os.path.join(second, file)
+            print(os.path.join(out_path, second))
+            try:
+                os.mkdir(os.path.join(out_path, second))
+            except FileExistsError:
+                pass
+            get_files(file_path, second)
         else:
-            print(file_path)
+            print(file)
+            fi = open(file_path, 'r', encoding=encode_in)
+            try:
+                data = fi.read()
+            except UnicodeDecodeError:
+                fi = open(file_path, 'r', encoding=encode_out)
+                data = fi.read()
+
+            filename_out = os.path.join(out_path, os.path.join(second, file))
+            with open(filename_out, mode='w', encoding=encode_out) as fo:
+                fo.write(data)
+                fo.close()
+
             list_name.append(file_path)
 
 
-get_files(path)
-print(list_name)
-
-# 输入文件的编码类型
-encode_in = 'utf-8'
-
-# 输出文件的编码类型
-encode_out = 'gb2312'
-for filename_in in list_name:
-    # with codecs.open(filename=filename_in, mode='r', encoding=encode_in) as fi:
-    fi = open(filename_in, 'r', encoding=encode_in)
-    try:
-        data = fi.read()
-    except UnicodeDecodeError:
-        fi = open(filename_in, 'r', encoding=encode_out)
-        data = fi.read()
-    filename_out = filename_in.replace("shuju", "dist")
-    with open(filename_out, mode='w', encoding=encode_out) as fo:
-        fo.write(data)
-        fo.close()
+get_files(path, "")
