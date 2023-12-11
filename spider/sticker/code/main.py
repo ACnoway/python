24be1fa0c27b45bdfaf9edea3ef5163ab26be316
ACnoway <imlzhyt@163.com>
2023-12-11 17:51:07 +0800

    spider\sticker\code/main

--- spider/sticker/code/main.py
+++ spider/sticker/code/main.py
@@ -16,23 +16,31 @@ def down(url, save, name):
     f.write(html)
 
 
-print("这是一个适用于 store.line.me 的表情包爬取工具，请将详情链接复制下来并粘贴，会下载到当前目录的dist文件夹内")
-print("请选择爬取类型：\n1. Stickers\n2. Emoji")
-patype = int(input())
-url = input("请输入要爬取的表情包链接：")
-html = requests.get(url, headers=headers, timeout=20).text
-if not os.path.exists("dist"):
-    os.mkdir("dist")
-data = BeautifulSoup(html, "html.parser")
-if patype == 1:
-    stickers = data.select(".mdCMN09Ul.FnStickerList li")
-else:
-    stickers = data.select(".mdCMN09Ul.FnEmoji_animation_list_img li")
-for sticker in stickers:
-    stimeta = json.loads(sticker.attrs["data-preview"])
-    staticp = stimeta["staticUrl"]
-    anip = stimeta["animationUrl"]
-    if stimeta["type"] == "static":
-        down(staticp, "dist", stimeta["id"])
+def main():
+    print("这是一个适用于 store.line.me 的表情包爬取工具，请将详情链接复制下来并粘贴，会下载到当前目录的dist文件夹内")
+    print("请选择爬取类型：\n1. Stickers\n2. Emoji")
+    patype = int(input())
+    url = input("请输入要爬取的表情包链接：")
+    try:
+        html = requests.get(url, headers=headers, timeout=20).text
+    except:
+        return
+    if not os.path.exists("dist"):
+        os.mkdir("dist")
+    data = BeautifulSoup(html, "html.parser")
+    if patype == 1:
+        stickers = data.select(".mdCMN09Ul.FnStickerList li")
     else:
-        down(anip, "dist", stimeta["id"])
+        stickers = data.select(".mdCMN09Ul.FnEmoji_animation_list_img li")
+    for sticker in stickers:
+        stimeta = json.loads(sticker.attrs["data-preview"])
+        staticp = stimeta["staticUrl"]
+        anip = stimeta["animationUrl"]
+        if stimeta["type"] == "static":
+            down(staticp, "dist", stimeta["id"])
+        else:
+            down(anip, "dist", stimeta["id"])
+
+
+main()
+input("按任意键退出")
