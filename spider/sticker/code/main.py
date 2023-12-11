54141ecd746e09f342460da2b4e66ec8a9c794d8
ACnoway <imlzhyt@163.com>
2023-12-11 16:20:16 +0800

    spider\sticker\code/main

--- spider/sticker/code/main.py
+++ spider/sticker/code/main.py
@@ -1,23 +1,38 @@
 import re
 import requests
 from bs4 import BeautifulSoup
-headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
-}
-url="https://www.biqugeg.cc/8_8465/26860982.html"
-html = requests.get(url,headers=headers).text
-#encode编码，将ISO-8859-1编码成unicode
-html=html.encode("GBK")
-data = BeautifulSoup(html,"html.parser")
-#获取每一章的章节名
-section_name = data.title.string
-print(section_name)
-#获取每一章节的文章内容
-section_text = data.select('#wrapper .book.reader .content #content.showtxt')[0].text
-#规范内容格式
-#re.sub( '\s+', '\r\n\t', section_text)指的是将内容中含有多个空格的地方替换为 回车（空行）+ tab缩进
-#.strip('\r\n')指的是将内容开头和结尾的空行
-section_text=re.sub( '\s+', '\r\n\t', section_text).strip('\r\n')
-print(section_text)
+import json
+import os
 
-# py spider\sticker\data
+headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+}
+
+
+def down(url, save, name):
+    html = requests.get(url, headers=headers, timeout=20).content
+    f = open(f"{save}/{name}.png", "wb")
+    f.write(html)
+
+
+print("这是一个适用于 store.line.me 的表情包爬取工具，请将详情链接复制下来并粘贴，会下载到当前目录的dist文件夹内")
+print("请选择爬取类型：\n1. Stickers\n2. Emoji")
+patype = int(input())
+url = input("请输入要爬取的表情包链接：")
+html = requests.get(url, headers=headers, timeout=20).text
+if not os.path.exists("dist"):
+    os.mkdir("dist")
+data = BeautifulSoup(html, "html.parser")
+if patype == 1:
+    stickers = data.select(".mdCMN09Ul.FnStickerList li")
+else:
+    stickers = data.select(".mdCMN09Ul.FnEmoji_animation_list_img li")
+for sticker in stickers:
+    stimeta = json.loads(sticker.attrs["data-preview"])
+    staticp = stimeta["staticUrl"]
+    anip = stimeta["animationUrl"]
+    if stimeta["type"] == "static":
+        down(staticp, "dist", stimeta["id"])
+    else:
+        down(anip, "dist", stimeta["id"])
